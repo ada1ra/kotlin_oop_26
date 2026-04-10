69611460a8e433f96891d589f2a05428b6ec8bee
ada1ra <adada0901@gmail.com>
2026-04-11 00:33:31 +0300

    Realize ListPrinter

--- src/main/kotlin/ListPrinter.kt
+++ src/main/kotlin/ListPrinter.kt
@@ -1,10 +1,7 @@
 package org.example
 
-import org.example.list.CustomArrayList
-import org.example.list.CustomList
-
 object ListPrinter {
-//    fun printList(list: ????) {
-//        implement
-//    }
+    fun printList(collection: Iterable<Int>) {
+        println(collection.joinToString(prefix = "[", postfix = "]"))
+    }
 }
