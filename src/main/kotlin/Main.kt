4b9557370937b93a9a495390cfa16217529ceee2
ada1ra <adada0901@gmail.com>
2026-04-11 00:34:08 +0300

    Add ListPrinter demonstration for another collections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
 package org.example
 
 import org.example.list.SingleLinkedList.Companion.singleLinkedListOf
+import org.example.list.CustomArrayList.Companion.customArrayListOf
+import org.example.stack.ArrayListStack.Companion.arrayListStackOf
+import org.example.stack.SingleLinkedStack.Companion.singleLinkedStackOf
 
 fun main() {
-    val t = singleLinkedListOf(1, 2, 3)
-//    ListPrinter.printList(t)
+    val sll = singleLinkedListOf(1, 2, 3)
+    val cal = customArrayListOf(4, 5, 6)
+    val als = arrayListStackOf(7, 8, 9)
+    val sls = singleLinkedStackOf(10, 11, 12)
+    ListPrinter.printList(sll)
+    ListPrinter.printList(cal)
+    ListPrinter.printList(als)
+    ListPrinter.printList(sls)
 }
